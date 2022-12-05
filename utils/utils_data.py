f545d444701a89d8ccd0e641ba6ece06a3862794
Abhijatmedhi Chotrattanapituk <40755638+c-earth@users.noreply.github.com>
2022-12-04 19:55:21 -0500

    Add files via upload

--- utils/utils_data.py
+++ utils/utils_data.py
@@ -1,6 +1,10 @@
 import os
 import glob
 import torch
+import numpy as np
+from mp_api.client import MPRester
+
+api_key = 'KKYDb56RGe2YQhuLJqE17k7AztkJ6Fyj'
 
 def load_raw(dat_dir):
     os.system(f'mkdir {dat_dir}zip/; wget -r -nd -A lzma http://phonondb.mtl.kyoto-u.ac.jp/_downloads/ -P {dat_dir}zip/')
@@ -11,11 +15,19 @@ def load_raw(dat_dir):
             os.system(f'mv {sub_dir} {sub_dir[:-10]}')
     os.system(f'rm -r ./temp/')
 
-def get_phonons(phn_dir):
+def get_reciprocal_lattice(id):
+    with MPRester(api_key) as mpr:
+        struct = mpr.get_structure_by_material_id(id)
+    return struct.lattice.reciprocal_lattice
+
+def get_phonons(phn_dir, cart_qpts):
     data_dict = dict()
     for sub_dir in glob.glob(f'{phn_dir}*/'):
         id = os.path.basename(os.path.normpath(sub_dir))
-        os.system(f'cp QPOINTS {sub_dir}; cd {sub_dir}; phonopy -p phonopy.conf -c POSCAR-unitcell --qpoints .TRUE.')
+        lattice = get_reciprocal_lattice(id)
+        qpts = lattice.get_fractional_coords(cart_qpts)
+        gen_qpts_file(qpts, 'QPOINTS')
+        os.system(f'mv QPOINTS {sub_dir}; cd {sub_dir}; phonopy -p phonopy.conf -c POSCAR-unitcell --qpoints .TRUE.')
         with open(f'{sub_dir}qpoints.yaml', 'r') as f:
             data = f.readlines()
         qpts = []
@@ -34,9 +46,22 @@ def get_phonons(phn_dir):
         data_dict[id] = [torch.tensor(qpts), torch.tensor(phonons)]
     return data_dict
 
-def gen_qpts_file(qpts):
+def gen_qpts_file(qpts, file_name):
     data = f'{len(qpts)}\n'
     for qpt in qpts:
         data += ' '.join(map(str, qpt)) + '\n'
-    with open('QPOINTS', 'w') as f:
-        f.write(data)
+    with open(file_name, 'w') as f:
+        data = f.write(data)
+
+def read_qpts_file(file_name):
+    with open(file_name, 'r') as f:
+        data = f.readlines()
+    qpts = []
+    for line in data:
+        cline = line.strip('\n').split()
+        if len(cline) == 3:
+            qpts.append([eval(c) for c in cline])
+    return qpts
+
+def gen_cart_qpts(L, n):
+    return L * np.random.uniform(0.0, 1.0, 3*n).reshape((-1, 3))
