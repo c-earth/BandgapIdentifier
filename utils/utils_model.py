22cb3634b205de70e8f352f269de55d2e9aba36e
Abhijatmedhi Chotrattanapituk <40755638+c-earth@users.noreply.github.com>
2022-12-09 15:27:34 -0500

    Correct indexing pivot

--- utils/utils_model.py
+++ utils/utils_model.py
@@ -1,4 +1,5 @@
 import torch
+import random
 
 dtype = torch.float64
 
@@ -7,20 +8,21 @@ class BandAugmentations():
         self.delta = delta
 
     def __call__(self, bands):
-        pivot = torch.randint(0, bands.shape[-1], (bands.shape[0], 1), device = bands.device)
-        inc_aug = bands + torch.abs(bands.normal(bands.zeros(), self.delta*bands.ones()))
-        dec_aug = bands - torch.abs(bands.normal(bands.zeros(), self.delta*bands.ones()))
+        pivot = [list(range(len(bands))),[random.randint(0, bands.shape[-1]-1)for i in range(len(bands))]]
+        inc_aug = bands + torch.abs(torch.normal(torch.zeros(bands.shape), self.delta*torch.ones(bands.shape)))
+        dec_aug = bands - torch.abs(torch.normal(torch.zeros(bands.shape), self.delta*torch.ones(bands.shape)))
         inc_aug[pivot] = bands[pivot]
         dec_aug[pivot] = bands[pivot]
         return torch.concat([inc_aug, dec_aug], dim = 0)
 
 class NCELoss(torch.nn.Module):
     def __init__(self, sim_fn, T = 1):
+        super(NCELoss, self).__init__()
         self.sim_fn = sim_fn
         self.T = T
 
-    def forward(self, batch, not_neg_mask, pos_mask):
-        sims = torch.masked_fill(self.sim_fn(batch) / self.T, not_neg_mask, -1e10)
+    def forward(self, features, not_neg_mask, pos_mask):
+        sims = torch.masked_fill(self.sim_fn(features) / self.T, not_neg_mask, -1e10)
         return torch.mean(-sims[pos_mask] + torch.logsumexp(sims, dim = -1))
 
 class FeatureNetwork(torch.nn.Module):
