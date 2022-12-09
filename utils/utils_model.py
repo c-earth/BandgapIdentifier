eeb103e9afcfe57451fa7e0bdd9b0da6b7134506
Abhijatmedhi Chotrattanapituk <40755638+c-earth@users.noreply.github.com>
2022-12-09 17:22:39 -0500

    Add Residual Network as feature network

--- utils/utils_model.py
+++ utils/utils_model.py
@@ -27,7 +27,7 @@ class NCELoss(torch.nn.Module):
         sims = torch.masked_fill(self.sim_fn(features) / self.T, not_neg_mask, -1e10)
         return torch.mean(-sims[pos_mask] + torch.logsumexp(sims, dim = -1))
 
-class FeatureNetwork(torch.nn.Module):
+class FeatureNetworkTmp(torch.nn.Module):
     def __init__(self, in_size, hidden1, hidden2, out_size):
         super().__init__()
         self.dropout = nn.Dropout(0.2)
@@ -42,6 +42,144 @@ class FeatureNetwork(torch.nn.Module):
         x = self.dropout(x)
         x = self.fc3(x)
         return x
+    
+def conv3x3x3(in_channels, out_channels, stride = 1, groups = 1, dilation = 1):
+    return nn.Conv3d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = dilation, groups = groups, bias = False, dilation = dilation)
+
+
+def conv1x1x1(in_channels, out_channels, stride = 1):
+    return nn.Conv3d(in_channels, out_channels, kernel_size = 1, stride = stride, bias = False)
+
+class Block(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels, channels, stride = 1, downsample = None, groups = 1, base_width = 64, dilation = 1, norm_layer = None):
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm3d
+        width = int(channels * (base_width / 64.0)) * groups
+        self.conv1 = conv1x1x1(in_channels, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1x1(width, channels * self.expansion)
+        self.bn3 = norm_layer(channels * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class FeatureNetwork(torch.nn.Module):
+    def __init__(self, layers, num_classes = 1000, zero_init_residual = False, groups =1, width_per_group = 64, replace_stride_with_dilation = None, norm_layer = None):
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm3d
+        self._norm_layer = norm_layer
+        self.in_channels = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                            f"or a 3-element tuple, got {replace_stride_with_dilation}")
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv3d(3, self.in_channels, kernel_size = 7, stride = 2, padding = 3, bias = False)
+        self.bn1 = norm_layer(self.in_channels)
+        self.relu = nn.ReLU(inplace = True)
+        self.maxpool = nn.MaxPool3d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer1 = self._make_layer(Block, 64, layers[0])
+        self.layer2 = self._make_layer(Block, 128, layers[1], stride = 2, dilate = replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(Block, 256, layers[2], stride = 2, dilate = replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(Block, 512, layers[3], stride = 2, dilate = replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool3d((1, 1))
+        self.fc = nn.Linear(512 * Block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.kaiming_normal_(m.weight, mode = "fan_out", nonlinearity = "relu")
+            elif isinstance(m, (nn.BatchNorm3d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Block) and m.bn3.weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)
+
+    def _make_layer(self, planes, blocks, stride = 1, dilate = False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.in_channels != planes * Block.expansion:
+            downsample = nn.Sequential(
+                conv1x1x1(self.in_channels, planes * Block.expansion, stride),
+                norm_layer(planes * Block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            Block(
+                self.in_channels, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
+            )
+        )
+        self.in_channels = planes * Block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                Block(
+                    self.in_channels,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, bands):
+        return self._forward_impl(bands)
 
 class ProjectionNetwork(torch.nn.Module):
     def __init__(self, n_features):
